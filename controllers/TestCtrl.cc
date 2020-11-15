4051fbe5baea204d469447c88a514f7888f52a3c
Matt Harrington <MattHarrington@users.noreply.github.com>
2020-11-15 14:23:58 -0800

    added .clang-format and example controller

---
+++ controllers/TestCtrl.cc
@@ -0,0 +1,5 @@
+#include "TestCtrl.h"
+void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    //write your application logic here
+}
